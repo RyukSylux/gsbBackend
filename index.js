51ca34ba2c61bc9b08df6a8110d0c1447db5b7ec
Morgan Bourre <momo.derien@gmail.com>
2025-04-08 16:47:05 +0200

    setup mongoDB

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const mongoose = require('mongoose')
+mongoose.connect('mongodb+srv://ryuk:ryuk@cluster0.q8tp4.mongodb.net/gsb')
+const db = mongoose.connection
+db.on('error', (err) => {console.log('Error connecting to MongoDB', err)})
+db.on('open', (err) => {console.log('connected to MongoDB')})
+
+app.use(express.json())
+
+app.get('/',(req,res) => {
+    res.send('<h1>connard</h1>')
+})
+
+app.listen(port, () =>{
+    console.log(`frr c'est sur http://127.0.0.1:${port}`)
+})
